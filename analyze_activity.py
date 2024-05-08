09603678562ab8d3c78c472ddb2746918fbd85c4
gretaangebrand <ag9716@mci4me.at>
2024-05-08 12:11:58 +0200

    Versuch Herzzonen

--- analyze_activity.py
+++ analyze_activity.py
@@ -20,5 +20,43 @@ df["Time_in_Seconds"] = df.index
 fig = px.line(df, x= "Time_in_Seconds", y= "PowerOriginal")
 fig.show()
 
+# %%
+#5 Zonen mittels Herzfrequenz einteilen
+print(df["HeartRate"])
+
+# %%
+
+# Angenommene maximale Herzfrequenz und Ruheherzfrequenz
+max_hr = 220 - age
+
+# Definition der Zonen
+def teile_in_herzfrequenz_zonen(herzfrequenz, max_hr):
+    zonen = {
+        "Zone 1": (0.5 * max_hr, 0.6 * max_hr),
+        "Zone 2": (0.6 * max_hr, 0.7 * max_hr),
+        "Zone 3": (0.7 * max_hr, 0.8 * max_hr),
+        "Zone 4": (0.8 * max_hr, 0.9 * max_hr),
+        "Zone 5": (0.9 * max_hr, max_hr)
+        }
+    
+    for zone, (min_hf, max_hf) in zonen.items():
+        if min_hf <= herzfrequenz < max_hf:
+            return zone
+    return "Außerhalb der Zonen"
+
+# Beispiel Herzfrequenzdaten
+df[""] = HeartRate
+
+# Einteilung der Herzfrequenz in Zonen
+heart_rate_zones = []
+for hr in heart_rates:
+    for zone, (lower, upper) in zones.items():
+        if lower <= hr <= upper:
+            heart_rate_zones.append((hr, zone))
+            break
+
+# Ausgabe der Herzfrequenzdaten mit zugehöriger Zone
+for hr, zone in heart_rate_zones:
+    print(f"Herzfrequenz: {hr}, Zone: {zone}")
 
 # %%
